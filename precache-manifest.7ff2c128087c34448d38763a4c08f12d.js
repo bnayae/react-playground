58099ac11d78688fcbd35ff176d362855b857e96
bnaya eshet <bnayanet@hotmail.com>
2019-01-09 14:19:23 +0200

    Updates

---
+++ precache-manifest.7ff2c128087c34448d38763a4c08f12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a27098d2c4a0e7981309",
+    "url": "/bnayae/react-playground/static/js/runtime~main.a27098d2.js"
+  },
+  {
+    "revision": "48f2948e3d83eacbd0de",
+    "url": "/bnayae/react-playground/static/js/main.48f2948e.chunk.js"
+  },
+  {
+    "revision": "11cd304093640138fb65",
+    "url": "/bnayae/react-playground/static/js/1.11cd3040.chunk.js"
+  },
+  {
+    "revision": "48f2948e3d83eacbd0de",
+    "url": "/bnayae/react-playground/static/css/main.83128a28.chunk.css"
+  },
+  {
+    "revision": "115540abd00b418554aa41cd6e45b5c8",
+    "url": "/bnayae/react-playground/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bnayae/react-playground/precache-manifest.7ff2c128087c34448d38763a4c08f12d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bnayae/react-playground/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
